e125062ab05097ef484f7a25d8b18d784d79ee2a
sunnibfgi <jamesanthony1523@163.com>
2015-11-26 15:15:23 +0800

    rebuild code

--- src/hash-tabs.js
+++ src/hash-tabs.js
@@ -1,6 +1,5 @@
 // hash-tabs.js
 // require jquery.js or zepto.js　modern library
-
 (function(global, $) {
   'use strict';
 
@@ -18,35 +17,50 @@
   }
     
   hashTabs.prototype.listener = function() {
-    $(this.el).on('hashtabs', $.proxy(this.hashChangeHandle, this));
-    $(window).on('hashchange', $.proxy(function() {
-      $(this.el).trigger('hashtabs');
-    }, this));
+    $(window).on('hashchange', $.proxy(this.setHashTabs, this));
     this.initialize();
   };
     
   hashTabs.prototype.initialize = function() {
     var opts = this.options;
-    var hash = location.hash.substring(1);
-    var hashContent = $('[data-hash=' + hash + ']', this.el);
-    $(this.el).trigger('hashtabs');
-    if (!hash || (hash && !hashContent.length)) {
+    this.clickHandler();
+    if (!location.hash.length || !this.setHashTabs()) {
       opts.idx = Math.max(0, Math.min(opts.idx, this.tab.length - 1));
-      hash = this.tab[opts.idx].hash.substring(1);
+      var href = this.tab[opts.idx].hash.substring(1);
       this.tab.eq(opts.idx).addClass('active');
-      $('[data-hash=' + hash + ']').removeClass('hide');
+      $('[data-hash=' + href + ']').removeClass('hide');
+    } else {
+      this.setHashTabs();
     }
   };
-    
-  hashTabs.prototype.hashChangeHandle = function(e) {
-    var hash = location.hash.substring(1);
-    var hashContent = $('[data-hash=' + hash + ']', this.el);
-    if (hashContent.length) {
-      this.content.addClass('hide');
-      hashContent.removeClass('hide');
-      this.tab.removeClass('active');
-      $('[href=#' + hash + ']').addClass('active');
-    }
+  
+  hashTabs.prototype.setHashTabs = function() {
+    var hash = location.hash;
+    var id = this.el.data('id');
+    var $this = this;
+    var correctHash = false;
+    $.each(this.tab, function(i, el) {
+      el = $(el);
+      var href = el.attr('href').substring(1);
+      if (('#' + id + '=' + href) === hash) {
+        $this.content.addClass('hide');
+        $('[data-hash=' + href + ']').removeClass('hide');
+        $this.tab.removeClass('active');
+        el.addClass('active');
+        correctHash = true;
+        return false;
+      }
+    });
+    return correctHash;
+  };
+  
+  hashTabs.prototype.clickHandler = function() {
+    var id = this.el.data('id');
+    this.tab.on('click', function(e) {　　
+      var href = $(this).attr('href').substring(1);
+      location.hash = id + '=' + href;
+      e.preventDefault();
+    });
   };
     
   window.hashTabs = hashTabs;
